598c91cd742ab0009df1dc3244756ef3bbc43999
VibrantSolace <85423035+VibrantSolace@users.noreply.github.com>
2026-03-21 23:42:26 -0700

    Update backest.js

--- backest.js
+++ backest.js
@@ -14,9 +14,29 @@ function counter() {
 
 }
 function animateBall() {
+
+    if (clicking) {
+        xSpd += (mouseX - 35 - xPos) * 0.002;
+        ySpd += (mouseY - 35 - yPos) * 0.002;
+    }
+    if (yPos + 70 > window.innerHeight) {
+        ySpd *= -1;
+        yPos = window.innerHeight - 71;
+    }
+    if (xPos < 0) {
+        xSpd *= -1;
+        xPos = 1;
+    }
+    else if (xPos + 70 > window.innerWidth) {
+        xSpd *= -1;
+        xPos = window.innerWidth - 71;
+    }
     ySpd += yAcc;
+    ySpd *= 0.99;
+    xSpd *= 0.995;    
     xPos += xSpd;
     yPos += ySpd;
+
     ball.style.translate = `${xPos}px ${yPos}px`;
     if (!stop){
     requestAnimationFrame(animateBall);
@@ -25,6 +45,22 @@ function animateBall() {
 function stopBall() {
     stop = true;
 }
+function down() {
+    pMouseX = mouseX;
+    pMouseY = mouseY;
+    clicking = true;
+}
+function release() {
+    clicking = false;
+}
+
+const display = document.getElementById("display");
+let mouseX = 0;
+let mouseY = 0;
+display.addEventListener("mousemove", (event) => {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+});
 
 let num = 0;
 const button = document.getElementById("myButton");
@@ -37,12 +73,15 @@ stopper.addEventListener("click", stopBall)
 
 const ball = document.getElementById("ball");
 const ballStyle = window.getComputedStyle(ball);
+let clicking = false;
+ball.addEventListener("mousedown", down);
+display.addEventListener("mouseup", release);
 
 let xPos = 0;
 let yPos = 0;
 let xSpd = 0;
 let ySpd = 0;
-let xAcc = 0;
-let yAcc = 0.02;
+let yAcc = 0.25;
 
 requestAnimationFrame(animateBall);
+
